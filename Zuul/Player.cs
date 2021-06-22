52582fcfdf7aaabedbbfe9fe1bffd682c25df350
DionKoenes <koenesdion@gmail.com>
2021-06-22 10:40:20 +0200

    Damage,Heal,isAlive

--- Zuul/Player.cs
+++ Zuul/Player.cs
@@ -5,10 +5,37 @@ namespace Zuul
     public class Player
     {
         public Room CurrentRoom { get; set; }
-
+        private int health;
+        private bool isAlive;
         public Player()
         {
             CurrentRoom = null;
+            health = 100;
+        }
+
+        public int Damage(int amount)
+        {
+            health = health - amount;
+            return amount;
+        }
+
+        public int Heal(int amount)
+        {
+            health = health + amount;
+            return amount;
+        }
+
+        public bool PlayerAlive()
+        {
+            if (health < 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            return isAlive;
         }
     }
 }
